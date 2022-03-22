c04b82c7744d0af3c7bf5c8aa10820c68cb48334
asthagangwar3 <76589160+asthagangwar3@users.noreply.github.com>
2022-03-22 15:48:25 +0530

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+C://Users//shwetakhai//AppData//Roaming//npm//appcenter login --token 58184fb70467860b7691eb5313f389cfeb9fb867
+C://Users//shwetakhai//AppData//Roaming//npm//appcenter profile list 
+C://Users//shwetakhai//AppData//Roaming//npm//appcenter apps list --token 58184fb70467860b7691eb5313f389cfeb9fb867 
+C://Users//shwetakhai//AppData//Roaming//npm//appcenter apps show --app android_poc
+C://Users//shwetakhai//AppData//Roaming//npm//appcenter distribute release --file app/build/outputs/apk/debug/*.apk --token 58184fb70467860b7691eb5313f389cfeb9fb867 --group android_group --app android_poc --debug
